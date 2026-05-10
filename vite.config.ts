81ebdf7fc11eb668de50373255bb1f4309e602ee
Telev <televo.management2@gmail.com>
2026-05-10 18:54:06 +0200

    aggiunta sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Sitemap({
+      hostname: 'https://ceraunavoltakids.vercel.app/',
+    }),
+  ],
 })
